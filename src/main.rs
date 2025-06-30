2fe00eb7e7c7986b2df6fafdd1d73478a0a9ee5c
luna <ellipticobj@gmail.com>
2025-06-30 21:00:35 +0800

    extracted variables

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,25 @@ use std::thread::sleep;
 use std::time::{Duration, Instant, SystemTime, UNIX_EPOCH};
 
 fn main() {
-    // TODO: custom icons?
+    // TODO: add a way to change these vars
     let faces = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
-    // TODO: custom animation duration?
-    let endtime = Instant::now() + Duration::from_secs_f32(0.6);
-    let interval = Duration::from_millis(10);
-    // TODO: animation toggle
-    while Instant::now() <= endtime {
-        let nanos = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_nanos();
-        let roll = (nanos % 6) as usize;
-        print!("\r{}", faces[roll]);
-        io::stdout().flush().unwrap();
-        sleep(interval);
+    let animdur: f32 = 0.6;
+    let dicesize = 6;
+    let animation = true;
+
+    if animation {
+        let endtime = Instant::now() + Duration::from_secs_f32(animdur);
+        let interval = Duration::from_millis(10);
+        while Instant::now() <= endtime {
+            let nanos = SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_nanos();
+            let roll = (nanos % 6) as usize;
+            print!("\r{}", faces[roll]);
+            io::stdout().flush().unwrap();
+            sleep(interval);
+        }
     }
 
     // generate pseudo random number using system time
@@ -26,7 +30,6 @@ fn main() {
         .unwrap()
         .as_nanos();
 
-    // TODO: custom dice
-    let finalroll = (nanos % 6) as usize;
+    let finalroll = (nanos % dicesize) as usize;
     println!("\r{} ({})", faces[finalroll], finalroll + 1);
 }
