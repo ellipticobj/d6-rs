a538ca1185a16225fb09a7c3d72b097228d7a659
luna <ellipticobj@gmail.com>
2025-06-30 20:55:16 +0800

    add future plans

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ use std::thread::sleep;
 use std::time::{Duration, Instant, SystemTime, UNIX_EPOCH};
 
 fn main() {
+    // TODO: custom icons?
     let faces = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
+    // TODO: custom animation duration?
     let endtime = Instant::now() + Duration::from_secs_f32(0.6);
     let interval = Duration::from_millis(10);
+    // TODO: animation toggle
     while Instant::now() <= endtime {
         let nanos = SystemTime::now()
             .duration_since(UNIX_EPOCH)
@@ -22,6 +25,8 @@ fn main() {
         .duration_since(UNIX_EPOCH)
         .unwrap()
         .as_nanos();
+
+    // TODO: custom dice
     let finalroll = (nanos % 6) as usize;
     println!("\r{} ({})", faces[finalroll], finalroll + 1);
 }
